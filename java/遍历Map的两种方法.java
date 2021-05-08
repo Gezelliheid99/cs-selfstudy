84c991bce6368bebacc91608af7206686b27bcaf
Gezelliheid99 <83463490+Gezelliheid99@users.noreply.github.com>
2021-05-08 20:31:00 +0800

    Update 遍历Map的两种方法.java

--- java/遍历Map的两种方法.java
+++ java/遍历Map的两种方法.java
@@ -9,30 +9,57 @@ import java.util.Set;
  */
 public class HashMapTest {
     public static void main(String[] args) {
-        Map<Integer, String> hm = new HashMap<>();
-        hm.put(1, "a");
-        hm.put(2, "b");
-        hm.put(3, "c");
-        hm.put(4, "d");
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(3, "c");
+        map.put(4, "d");
 
-        //第一种方式：构造器遍历
+        //第一种方式
         //获取所有的key，所有的keys是一个set集合
-        Set<Integer> keys = hm.keySet();
+        Set<Integer> keys = map.keySet();
         //set集合调用iterator，拿到构造器
         Iterator<Integer> it = keys.iterator();
-
+        
+        //遍历key 就可有拿到每一个key对应的value
         while (it.hasNext()){
             Integer key =it.next();
+            
             //map调用get(key)方法，拿到key对应的value
-            String value = hm.get(key);
+            String value = map.get(key);
+            
             System.out.println(key +" : " + value);
         }
 
         System.out.println("===========================");
-        
-        //用foreach也可以
+
+        //用foreach也可以，返回变量为 key
         for (Integer key : keys) {
-            System.out.println(key + " : " + hm.get(key));
+            System.out.println(key + " : " + map.get(key));
+        }
+
+        System.out.println("===================================");
+
+        //第二种方式 Set<Map.Entry<K, V>>  entrySet()
+        //以上这个集合是把map集合全部转换成set集合
+        //Set集合中的类型是 Map.entry
+        //构造器遍历
+        Set<Map.Entry<Integer, String>> set = map.entrySet();
+        Iterator<Map.Entry<Integer, String>> it2 = set.iterator();
+        while (it2.hasNext()){
+            //node 里面包含了key 和 value
+            //类型为 Map.Entry<Integer, String>
+             Map.Entry<Integer, String> node = it2.next();
+             Integer key = node.getKey();
+             String value = node.getValue();
+             System.out.println(key + " : " + value);
+        }
+
+        System.out.println("============================");
+        //foreach 遍历
+        for (Map.Entry<Integer, String> integerStringEntry : set) {
+            System.out.println(integerStringEntry.getKey() + ":" + integerStringEntry.getValue());
         }
     }
+
 }
